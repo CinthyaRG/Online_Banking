19e95dd1fa774d40506108da2f95f533c33f8ffb
cinthyaramos <09-11237@usb.ve>
2017-06-26 12:07:13 -0400

    pantalla de tarjeta de coordenadas casi lista

--- Online_Banking/static/js/script-request.js
+++ Online_Banking/static/js/script-request.js
@@ -7,6 +7,7 @@ $(document).ready(function (){
     drop_num_cheq();
     drop_agen();
     drop_account();
+    tarj_coor();
 
 });
 
@@ -59,6 +60,50 @@ function drop_account(){
     $.each(account,function (i,val) {
            $("#account").append('<option value="'+val+'"> '+val+'</option>');
     })
+}
+
+
+function tarj_coor() {
+    var c= "";
+    var i, j;
+    // $.getJSON('static/js/answer.json', function (data) {
+    //           $.each( data, function( key, val ) {
+    //               $("#anwers").append('<span class="h4 answer_help"><i class="fa fa-caret-right"> </i>'+
+    //                         '<span>'+ val.pregunta +'</span></span>'+
+    //                         '<p class="margin-l-45 text-wrapper">'+ val.respuesta +'</p>');
+    //           });
+    //         });
+
+    c+='<table>';
+    c+='<tr>';
+
+
+    c+='<td align="center">';
+    c+='<table>';
+    c+='<tr><td></td>'+'<td align="center"><strong>A</strong></td>'+
+    '<td align="center"><strong>B</strong></td>' +
+    '<td align="center"><strong>C</strong></td>' +
+    '<td align="center"><strong>D</strong></td>' +
+    '<td align="center"><strong>E</strong></td>' +
+    '<td align="center"><strong>F</strong></td></tr>';
+    for ( i = 0; i < 5; i++) {
+        c+='<tr><td class="col-xs-1" align="center"><strong>'+(i+1)+'</strong></td>';
+        for ( j = 0; j < 6; j++) {
+            c+='<td><input type="text" size="1" maxlength="3"/></td>';
+        }
+        c+='</tr>';
+    }
+    c+='</table>';
+    c+='</td>';
+
+    c+='</tr>';
+    c+='</table>';
+    c+='<div class="row"><div class="col-md-3">' +
+        '</div><div class="col-md-6"><p class="pull-right"> Serial 1234567</p></div>'+
+        '</div>';
+
+    $('#tarj-coor').append(c);
+
 }
 
 
--- Online_Banking_App/views.py
+++ Online_Banking_App/views.py
@@ -3,109 +3,125 @@ from django.views.generic import *
 
 
 class Home(TemplateView):
-    template_name = 'base-index.html'
+  template_name = 'base-index.html'
 
 
 class Home_Client(TemplateView):
-    template_name = 'base.html'
+  template_name = 'base.html'
 
 
 class Logout(TemplateView):
-    template_name = 'logout.html'
+  template_name = 'logout.html'
 
 
 class Register_success(TemplateView):
-    template_name = 'register_success.html'
+  template_name = 'register_success.html'
 
 
 class Restore_pass_success(TemplateView):
-    template_name = 'restore_pass_success.html'
+  template_name = 'restore_pass_success.html'
 
 
 class Register(TemplateView):
-    template_name = 'register.html'
+  template_name = 'register.html'
 
 
 class Restore_pass(TemplateView):
-    template_name = 'forgot_password.html'
+  template_name = 'forgot_password.html'
 
 
 class Account(TemplateView):
-    template_name = 'accounts.html'
+  template_name = 'accounts.html'
 
 
 class Tdc(TemplateView):
-    template_name = 'tdc.html'
+  template_name = 'tdc.html'
 
 
 class Loans(TemplateView):
-    template_name = 'loans.html'
+  template_name = 'loans.html'
 
 
 class Transfer_my_acc(TemplateView):
-    template_name = 'trans_my_acc.html'
+  template_name = 'trans_my_acc.html'
 
 
 class Transfer_my_bank(TemplateView):
-    template_name = 'trans_my_bank.html'
+  template_name = 'trans_my_bank.html'
 
 
 class Transfer_others_bank(TemplateView):
-    template_name = 'trans_other_bank.html'
+  template_name = 'trans_other_bank.html'
 
 
 class DataTransfer(TemplateView):
-    template_name = 'transfer-data.html'
+  template_name = 'transfer-data.html'
 
 
 class Success(TemplateView):
-    template_name = 'transfer-success.html'
+  template_name = 'transfer-success.html'
 
 
 class Payments(TemplateView):
-    template_name = 'payments.html'
+  template_name = 'payments.html'
 
 
 class DataPayment(TemplateView):
-    template_name = 'payment-data.html'
+  template_name = 'payment-data.html'
 
 
 class Success_Payments(TemplateView):
-    template_name = 'payments-success.html'
+  template_name = 'payments-success.html'
 
 
 class Register_Affiliate(TemplateView):
-    template_name = 'register-affiliate.html'
+  template_name = 'register-affiliate.html'
 
 
 class Register_Services(TemplateView):
-    template_name = 'register-services.html'
+  template_name = 'register-services.html'
 
 
 class Request(TemplateView):
-    template_name = 'request.html'
+  template_name = 'request.html'
+
+
+class Request_Coord(TemplateView):
+  template_name = 'req-tar-coor.html'
 
 
 class Request_Checkbook(TemplateView):
-    template_name = 'req-checkb.html'
+  template_name = 'req-checkb.html'
 
 
 class Request_Appointment(TemplateView):
-    template_name = 'req-appointment.html'
+  template_name = 'req-appointment.html'
 
 
 class Request_References(TemplateView):
-    template_name = 'req-reference.html'
+  template_name = 'req-reference.html'
+
+
+class Request_References_Success(TemplateView):
+  template_name = 'req-reference_success.html'
+
+
+class Request_Appointment_Success(TemplateView):
+  template_name = 'req-appointment_success.html'
+
+
+class Request_Checkbook_Success(TemplateView):
+  template_name = 'req-checkb_success.html'
 
 
 class Management(TemplateView):
-    template_name = 'management-products.html'
+  template_name = 'management-products.html'
 
 
 class Profile(TemplateView):
-    template_name = 'profile-security.html'
+  template_name = 'profile-security.html'
 
 
 class Help(TemplateView):
-    template_name = 'help.html'
+  template_name = 'help.html'
 
