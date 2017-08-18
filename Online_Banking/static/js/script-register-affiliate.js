9ac3447a2c6c058ee198e013e51357232f8b0434
Cinthya Ramos <09-11237@usb.ve>
2017-08-18 09:19:15 -0400

    validaciones en la afiliacion de beneficiarios

--- Online_Banking/static/js/script-register-affiliate.js
+++ Online_Banking/static/js/script-register-affiliate.js
@@ -49,28 +49,74 @@ function drop_bank(){
 function validate_affiliate(a,b,c,d,e,f,g,h) {
     var errors = false;
     var regexEmail = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,3})$/i;
+    var regexLetters = /^[a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑüÜ\s ]*$/;
 
     if ($(a).val() === '0') {
         notification_error('Seleccione un banco de la lista.');
         $(a).addClass('errors');
+        errors = true;
     }
     if ($(b).val().length < 20) {
-        notification_error('El número de cuenta debe tener 20 dígitos.');
-        $(a).addClass('errors');
+        notification_error('El número de cuenta debe contener 20 dígitos.');
+        $(b).addClass('errors');
+        errors = true;
     }
     if (isNaN($(b).val())) {
         notification_error('Solo se admiten números en el número de cuenta.');
-        $(a).addClass('errors');
+        $(b).addClass('errors');
+        errors = true;
+    }
+    if ($(c).val()  === ' '){
+        notification_error('El nombre del afiliado no puede estar vacío.');
+        $(c).addClass('errors');
+        errors = true;
+    }
+    if ($(c).val().match(regexLetters)){
+        notification_error('El nombre del afiliado solo admite letras.');
+        $(c).addClass('errors');
+        errors = true;
+    }
+    if ($(d).val().includes('V') || $(d).val().includes('E')) {
+        if ($(e).val().length < 8) {
+            notification_error('El documento de identidad debe contener 8 dígitos.');
+            $(e).addClass('errors');
+            errors = true;
+        }
+    }
+    if ($(d).val().includes('J')) {
+        if ($(e).val().length < 9) {
+            notification_error('El documento de identidad debe contener 8 dígitos.');
+            $(e).addClass('errors');
+            errors = true;
+        }
+    }
+    if (isNaN($(e).val())) {
+        notification_error('Solo se admiten números en el documento de identidad.');
+        $(e).addClass('errors');
+        errors = true;
+    }
+    if ($(f).val()  === ' '){
+        notification_error('El alias no puede estar vacío.');
+        $(f).addClass('errors');
+        errors = true;
+    }
+    if ($(f).val().match(regexLetters)){
+        notification_error('El alias solo admite letras.');
+        $(f).addClass('errors');
+        errors = true;
     }
     if (!($(g).val().match(regexEmail))) {
         notification_error('Ingrese un email válido.');
         $(a).addClass('errors');
+        errors = true;
     }
     if ($(h).val() !== $(g).val()) {
-        notification_error('Seleccione un banco de la lista.');
-        $(a).addClass('errors');
+        notification_error('La confirmación debe ser igual al email.');
+        $(h).addClass('errors');
+        errors = true;
     }
 
+    return errors
 }
 
 function add_affiliate(a,b,c,d,e,f,g,h) {
